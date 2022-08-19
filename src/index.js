edb9f0e98e7900e92f7a5df674e2f918af37ec7d
andreachicco <andreachicco2002.ac@gmail.com>
2022-08-19 10:46:23 +0200

    increase scalability by distributing server on more cpu cores

--- src/index.js
+++ src/index.js
@@ -1,36 +1,51 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const morgan = require('morgan');
-const { database } = require('./mongoose');
+const os = require('os');
+const cluster = require('cluster');
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const dbUri = process.env.DB_URI;
-database.connectDatabase(dbUri);
+if(cluster.isMaster) {
+  let nCpus = os.cpus().length;
 
-const io = require('./socket');
+  //Check if the inserted env var is ok 
+  if(process.env.NCPUS <= nCpus) nCpus = process.env.NCPUS;
 
-const homeRoute = require('./routes/home.route');
-const roomRoute = require('./routes/room.route');
-
-const app = express();
-
-app.use(morgan('dev'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }))
-
-const server = http.createServer(app);
-io.init(server);
-
-app.use('/', homeRoute);
-app.use('/room', roomRoute);
-
-const serverPort = process.env.PORT;
-
-server.listen(serverPort, () => {
-  console.log(`Listening on *:${serverPort}`);
-});
+  console.log(`Forking on ${nCpus} CPUs`);
+  for(let i = 0; i < nCpus; ++i) cluster.fork();
+}
+else {
+  const express = require('express');
+  const path = require('path');
+  const http = require('http');
+  const morgan = require('morgan');
+  const { database } = require('./mongoose');
+  
+  const dbUri = process.env.DB_URI;
+  database.connectDatabase(dbUri);
+  
+  const io = require('./socket');
+  
+  const homeRoute = require('./routes/home.route');
+  const roomRoute = require('./routes/room.route');
+  
+  const app = express();
+  
+  app.use(morgan('dev'));
+  
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  app.use(express.urlencoded({ extended: true }))
+  
+  const server = http.createServer(app);
+  io.init(server);
+  
+  app.use('/', homeRoute);
+  app.use('/room', roomRoute);
+  
+  const serverPort = process.env.PORT;
+  
+  const pid = process.pid;
+  server.listen(serverPort, () => {
+    console.log(`Server: process ${pid} is listening on *:${serverPort}`);
+  });
+}
