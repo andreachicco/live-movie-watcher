bc694642392f39df55cf5385e78dfec51cdaf6fd
is_andreachicco <andreachicco2002.ac@gmail.com>
2024-07-02 10:23:05 +0100

    add ws recv implementation from scratch

--- server/controllers/room.controller.go
+++ server/controllers/room.controller.go
@@ -3,11 +3,38 @@ package controllers
 import (
 	"fmt"
 	"net/http"
-	"wemovie/common"
+	"os"
+	"wemovie/ws"
 )
 
 func Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create room")
-	response := common.Response[int]{Status: common.CREATED, Message: "Room created succesfully", Data: nil}
-	response.Send(w)
+	// user := r.Context().Value("user").(*models.User)
+	ws, err := ws.New(w, r.Header)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error()+"\n")
+		return
+	}
+
+	err = ws.Handshake()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error()+"\n")
+		return
+	}
+
+	defer ws.Close()
+
+	for {
+		frame, err := ws.Recv()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, err.Error()+"\n")
+			break
+		}
+
+		fmt.Println(string(frame.Payload))
+
+		if frame.OpCode == 8 {
+			break
+		}
+	}
 }
